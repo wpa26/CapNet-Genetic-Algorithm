8897cc780da486ace6ff862fab1e68fc43e37aed
William Abajian <wpa26@cornel.edu>
2017-12-09 22:56:44 -0800

    Adding some variables

---
+++ source.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+int numGens = 10;
+int numSpecies = 100;
+float probCrossover = 0.25;
+float probPoint = 0.01;
+int numCaps;
+float caps[] = { 100, 150, 220, 330, 470, 680, 1 }; //caps in picofarads
+
+
+
+main(){
+
+}

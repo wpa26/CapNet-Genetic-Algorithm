ff47db0c60d4dc27f6ba249f1b1b7d84137b2acf
William Abajian <wpa26@cornel.edu>
2017-12-09 23:25:21 -0800

    Adding some organization

--- source.c
+++ source.c
@@ -3,14 +3,39 @@
 
 
 int numGens = 10;
-int numSpecies = 100;
+int numOrganisms = 100;
 float probCrossover = 0.25;
 float probPoint = 0.01;
 int numCaps;
-float caps[] = { 100, 150, 220, 330, 470, 680, 1 }; //caps in picofarads
+float caps[] = { 100, 150, 220, 330, 470, 680, 1000 }; //caps in picofarads
+float goalCap = 425; //Goal capacitance in picofards
 
 
+typedef struct Organisms {
+	chromosome genes;
+	float Fitness; //difference from goal
+	float lowerBound;
+	float upperBound;
+} Organism;
+
+
+Organism orgs[numOrganisms];
 
 main(){
+	int i;
+	//Generate Populations
+	float rouletteWheelLength;
+	//Run Generations
+	for (i = 0; i < numGens; ++i) {
+		rouletteWheelLength = 0;
+		for(j = 0; j < numOrganisms; ++j){
+			//Determine Fitness and start and end
+			orgs[]
+
+		}
+		//Each Organism has a start and end on roulette wheel
+	}
+
+
 
 }

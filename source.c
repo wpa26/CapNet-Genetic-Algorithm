f2bda0a847df43aa4c7ad35dc6c72b073e1c3276
wpa26 <wpa26@cornell.edu>
2018-03-18 18:07:58 -0700

    Finished the string formating for the chromosomes, which will be used for both human readability, and parsing for capacitance calculation

--- source.c
+++ source.c
@@ -6,19 +6,25 @@
 
 #define NUM_GENS 10
 #define NUM_ORGANISMS 100
-#define CHROM_MASK 0x1FFFFFFFF
-#define GENE_LENGTH 37
-
+#define CHROM_MASK 0xFFFFFFF
+#define GENE_LENGTH 28
+#define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
 
 float probCrossover = 0.0075; // 25% of any crossover
 float probPoint = 0.01;
 int numCaps = 5; //maximum number of caps
 float caps[] = { 100, 150, 220, 330, 470, 680, 1000 }; //caps in picofarads
+int capIndex[] = {3,10,16,21,25};
+int stringIndex[] = {3, 11, 19, 27, 35};
+int opIndex[] = {6, 13, 19, 24};
+int opStringIndex[] = {7, 16, 25, 34};
 float goalCap = 425; //Goal capacitance in picofards
 
 
 // (((C1 op (((C2) op ((C3)) op (C4))) op C5)))
-typedef long chromosome; //chromosome takes up 37 bits in 5
+// Ok never mind we only need to use half the parenthesis
+// So only open parenthesis are encoded
+typedef int chromosome; //chromosome takes up 37 bits in 5
 
 typedef struct Organisms {
 	chromosome genes;
@@ -94,11 +100,92 @@ void mate( Organism * parent1, Organism * parent2, Organism * child1, Organism *
 }
 
 void printChromosome( Organism individual ){
+	char string[100];
+	char temp[5];
+	int length = 0;
+	int longest_cap = 4;
+	float cap_val;
+	chromosome gene = individual.genes;
+	chromosome mask = 7;
+	chromosome index;
+	int i,j;
+	for(i = 0; i < 42;++i){
+		string[i] = ' ';
+	}
+	string[42] = '\0';
+	if(CHECK_BIT(gene,0)){
+		string[0] = '(';
+		string[33] = ')';
+	}
+	if(CHECK_BIT(gene,1)){
+		string[1] = '(';
+		string[24] = ')';
+	}
+	if(CHECK_BIT(gene,2)){
+		string[2] = '(';
+		string[15] = ')';
+	}
+	if(CHECK_BIT(gene,7)){
+		string[8] = '(';
+		string[41] = ')';
+	}
+	if(CHECK_BIT(gene,8)){
+		string[9] = '(';
+		string[32] = ')';
+	}
+	if(CHECK_BIT(gene,9)){
+		string[10] = '(';
+		string[23] = ')';
+	}
+	if(CHECK_BIT(gene,14)){
+		string[17] = '(';
+		string[40] = ')';
+	}
+	if(CHECK_BIT(gene,15)){
+		string[18] = '(';
+		string[31] = ')';
+	}
+	if(CHECK_BIT(gene,20)){
+		string[26] = '(';
+		string[39] = ')';
+	}
+	for(j = 0; j < 5; ++j){
+		index = (gene & (mask << capIndex[j]))>>capIndex[j];
+		if(index){
+			cap_val = caps[index - 1];
+			sprintf(temp,"%04.f",cap_val);
+			for(i = 0;i<4;++i){
+				string[stringIndex[j]+i] = temp[i];
+			}
+		}
+		else{
+			for(i = 0;i<4;++i){
+				string[stringIndex[j]+i] = '0';
+			}
+		}
+	}
+	for(j = 0; j < 4; ++j){
+		if(CHECK_BIT(gene, opIndex[j])){
+			string[opStringIndex[j]] = '+'; //1 is series
+		}
+		else{
+			string[opStringIndex[j]] = '|'; //0 is parallel
+		}
+	}
+
+
+	printf("%s\n",string);
 	return;
 }
 
 int main(){
-	int i,j;
+	Organism example;
+	example.genes = 0b0101010101000111001101001110;
+	printChromosome(example);
+	example.genes = 0b1111111111111111111111111111;
+	printChromosome(example);
+
+	/*int i,j;
 	float rouletteWheelLength;
 	//http://www.dummies.com/programming/c/how-to-generate-random-numbers-in-c-programming/
 	srand((unsigned)time(NULL));
@@ -168,7 +255,7 @@ int main(){
 	}
 	//Print out solution
 	printChromosome(bestGlobalSolution);
-
+	*/
 
 
 }
